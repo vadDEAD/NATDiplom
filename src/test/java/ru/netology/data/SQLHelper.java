f16f48b19f36fcb4c8cd3095443c17743d5907cc
vaDEAD <t0rm0z.vad.tru@gmail.com>
2023-06-14 00:26:22 +0300

    14.06

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -1,15 +1,57 @@
 package ru.netology.data;
 
-import java.sql.Connection;
+import lombok.val;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLHelper {
-    public static Connection getConnection() throws SQLException {
-        String url = System.getProperty("sut.url");
-        String login = System.getProperty("db.user");
-        String password = System.getProperty("db.password");
-        final Connection connection = DriverManager.getConnection(url, login, password);
-        return connection;
+    private static String url= System.getProperty("db.url");
+    private static String user = System.getProperty("db.user");
+    private static String password = System.getProperty("db.password");
+
+    public static void CleanUp() {
+        val deletePaymentEntity = "DELETE FROM payment_entity";
+        val deleteCreditEntity = "DELETE FROM credit_request_entity";
+        val deleteOrderEntity = "DELETE FROM order_entity";
+        val runner = new QueryRunner();
+        try (val conn = DriverManager.getConnection(
+                url, user, password)
+        ) {
+            runner.update(conn, deletePaymentEntity);
+            runner.update(conn, deleteCreditEntity);
+            runner.update(conn, deleteOrderEntity);
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+
+    }
+
+    public static String getPayStatus() throws SQLException {
+        String statusSQL = "SELECT status FROM payment_entity";
+        return getStatus(statusSQL);
+    }
+
+    public static String getCreditStatus() throws SQLException {
+        String statusSQL = "SELECT status FROM credit_request_entity";
+        return getStatus(statusSQL);
+    }
+
+    private static String getStatus(String query) throws SQLException {
+        String result = "";
+        val runner = new QueryRunner();
+        try
+                (val conn = DriverManager.getConnection(
+                        url, user, password)
+                ) {
+
+            result = runner.query(conn, query, new ScalarHandler<String>());
+            System.out.println(result);
+            return result;
+        }
+
     }
 }
--- src/test/java/ru/netology/tests/TourPurchaseTest.java
+++ src/test/java/ru/netology/tests/TourPurchaseTest.java
@@ -1,4 +1,55 @@
 package ru.netology.tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.*;
+import ru.netology.page.PurchasePage;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.data.DataGenerator.*;
+import static ru.netology.data.SQLHelper.*;
+
 public class TourPurchaseTest {
+
+    @BeforeAll
+    public static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    void setUP() {
+        String sutUrl = System.getProperty("sut.url");
+        open(sutUrl);
+    }
+
+
+    @AfterAll
+    public static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @Nested
+    //"Валидная карта"
+
+    public class ValidCard {
+
+        @Test
+        @SneakyThrows
+        @DisplayName("Покупка валидной картой")
+        public void shouldPaymentValidCard() {
+            var purchasePage = new PurchasePage();
+            purchasePage.cardPayment();
+            var info = getApprovedCard();
+            purchasePage.sendingData(info);
+            TimeUnit.SECONDS.sleep(10);
+            var expected = "APPROVED";
+            var payStatus = getPayStatus();
+            assertEquals("APPROVED", payStatus);
+        }
+
+    }
 }
